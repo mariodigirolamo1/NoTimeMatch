b22b4c4729b65cb41850c55a4fbc222a44c9f2bb
mariodigirolamo1 <mariodigirolamowork@gmail.com>
2024-02-01 21:49:18 +0100

    feat: add home composable
    add navigation buttons to home

---
+++ app/src/main/java/com/mdg/notimematch/home/Home.kt
@@ -0,0 +1,21 @@
+package com.mdg.notimematch.home
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Home() {
+    Column {
+        Button({}){
+            Text("Closet")
+        }
+        Button({}){
+            Text("Matches")
+        }
+        Button({}){
+            Text("Re-Match")
+        }
+    }
+}
