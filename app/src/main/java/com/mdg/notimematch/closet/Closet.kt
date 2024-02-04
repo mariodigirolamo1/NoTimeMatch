317506b3ca4e7b697eedfec9b7a6d003154e1579
mariodigirolamo1 <mariodigirolamowork@gmail.com>
2024-02-04 11:39:18 +0100

    test: add shown labels test on closet navigation
    extract labels string to resources

--- app/src/main/java/com/mdg/notimematch/closet/Closet.kt
+++ app/src/main/java/com/mdg/notimematch/closet/Closet.kt
@@ -18,9 +18,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.mdg.notimematch.model.GarmentTypes
+import com.mdg.notimematch.model.GarmentType
 import com.mdg.notimematch.ui.theme.NoTimeMatchTheme
 
 @Composable
@@ -32,7 +33,7 @@ fun Closet() {
 @Composable
 private fun Categories(){
     LazyColumn{
-        GarmentTypes.values().forEach { garmentType ->
+        GarmentType.values().forEach { garmentType ->
             val garmentTypeValue = garmentType.value
 
             item(key = garmentTypeValue){
@@ -41,8 +42,9 @@ private fun Categories(){
                         .fillMaxWidth()
                         .padding(all = 20.dp),
                 ) {
+                    val garmentTypeRes = GarmentType.getStringResourceForType(garmentType)
                     Text(
-                        text = garmentTypeValue,
+                        text = stringResource(id = garmentTypeRes),
                         style = MaterialTheme.typography.titleLarge
                     )
                     Spacer(modifier = Modifier.height(10.dp))
--- app/src/main/java/com/mdg/notimematch/model/GarmentTypes.kt
+++
@@ -1,8 +0,0 @@
-package com.mdg.notimematch.model
-
-enum class GarmentTypes(val value: String) {
-    SHOES(value = "SHOES"),
-    PANTS(value = "PANTS"),
-    SHIRT(value = "SHIRT"),
-    JACKET(value = "JACKET")
-}
